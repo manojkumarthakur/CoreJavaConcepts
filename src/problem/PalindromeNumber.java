6bcf6f8840318e69075ea039988903c121a24971
manojkumarthakur <manoj.test.acc@gmail.com>
2025-08-20 19:55:40 +0530

    palindrome for number

---
+++ src/problem/PalindromeNumber.java
@@ -0,0 +1,37 @@
+package problem;
+
+public class PalindromeNumber {
+    public static void main(String[] args) {
+       boolean isPal =  isPalindrome(121);
+        System.out.println(isPal);
+    }
+
+    public static boolean isPalindrome(int x){
+        int temp = x;
+        int rev = 0 ;
+        while (x>0){
+            rev = rev*10 + x%10;
+            x=x/10;
+        }
+        System.out.println("temp: " + temp);
+        System.out.println("rev: " + rev);
+        if(rev == temp)
+            return true;
+        else
+            return false;
+
+//         String s = new String(Integer.toString(x));
+//         StringBuilder str = new StringBuilder(s);
+//         String reverse = str.reverse().toString();
+//         System.out.println("s: " + s);
+//         System.out.println("reverse: " + str);
+//         if(s.equals(reverse))
+//             return true;
+//         else
+//             return false;
+    }
+
+
+}
+
+
