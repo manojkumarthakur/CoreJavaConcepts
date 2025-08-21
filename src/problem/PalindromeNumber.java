8256ee5420125a24bbc5bc01e9dbc8f4d4cbe98c
manojkumarthakur <manoj.test.acc@gmail.com>
2025-08-21 21:45:17 +0530

    more problems

--- src/problem/PalindromeNumber.java
+++ src/problem/PalindromeNumber.java
@@ -2,13 +2,15 @@ package problem;
 
 public class PalindromeNumber {
     public static void main(String[] args) {
-       boolean isPal =  isPalindrome(121);
+       boolean isPal =  isPalindrome(-121);
         System.out.println(isPal);
     }
 
     public static boolean isPalindrome(int x){
         int temp = x;
         int rev = 0 ;
+        if(x<0)
+            return false;
         while (x>0){
             rev = rev*10 + x%10;
             x=x/10;
---
+++ src/problem/ValidParentheses.java
@@ -0,0 +1,51 @@
+package problem;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+    public static boolean isValid(String s) {
+        char[] arr = s.toCharArray();
+        Stack<Character> stack = new Stack<>();
+
+        if(arr.length % 2 == 1)
+            return false;
+        for(char c : arr){
+            if (c == ')' || c == '}' || c == ']'){
+                if(stack.empty())
+                    return false;
+                char popValue = stack.pop();
+                if((c==')' && popValue=='(') || (c=='}' && popValue=='{') || (c==']' && popValue=='['))
+                    continue;
+                else
+                    return false;
+            } else {
+                stack.push(c);
+            }
+
+        }
+        return stack.size() == 0;
+    }
+
+    public static boolean isValidSol(String s) {
+        Stack<Character> paren= new Stack<>();
+        for(char c:s.toCharArray()){
+            if (c == '(') {
+                paren.push(')');
+            } else if (c == '{') {
+                paren.push('}');
+            } else if (c == '[') {
+                paren.push(']');
+            } else {
+                if (paren.isEmpty() || paren.pop() != c) {
+                    return false;
+                }
+            }
+        }
+        return paren.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isValid("([]{})"));
+        System.out.println(isValidSol("([]{})"));
+    }
+}
